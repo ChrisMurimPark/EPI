598eac8c248abecd0c9d9a8ba7d213537e0e87d2
Chris (Murim) Park <chrispark@wustl.edu>
2018-05-15 19:38:10 -0500

    Moving non EPI things into its own folder

--- UnitTests/NonEPI/BFSTests.cpp
+++ UnitTests/NonEPI/BFSTests.cpp
@@ -1,22 +1,22 @@
 //
-//  DFSTests.cpp
+//  BFSTests.cpp
 //  EPI
 //
-//  Created by Chris (Murim) Park on 4/29/18.
+//  Created by Chris (Murim) Park on 5/1/18.
 //  Copyright © 2018 Chris (Murim) Park. All rights reserved.
 //
 
 #include "gtest/gtest.h"
-#include "../../EPI/Graphs/General/DFS.hpp"
+#include "../../EPI/NonEPI/BFS/BFS.hpp"
 
-TEST(DFSTests, DFSRecursive_GivenEmptyGraph_ReturnsEmptyPath)
+TEST(BFSTests, BFS_GivenEmptyGraph_ReturnsEmptyPath)
 {
     std::unordered_map<int, std::set<int>> graph;
-    const std::vector<int> &path = DFSRecursive(graph, 0, 1);
+    const std::vector<int> &path = BFS(graph, 0, 1);
     EXPECT_EQ(0, path.size());
 }
 
-TEST(DFSTests, DFSRecursive_GivenValidGraph_ReturnsCorrectPath)
+TEST(BFSTests, BFS_GivenValidGraph_ReturnsCorrectPath)
 {
     std::unordered_map<int, std::set<int>> graph;
     graph.emplace(std::pair<int, std::set<int>>(1, {2, 3, 6}));
@@ -25,11 +25,9 @@ TEST(DFSTests, DFSRecursive_GivenValidGraph_ReturnsCorrectPath)
     graph.emplace(std::pair<int, std::set<int>>(4, {5}));
     graph.emplace(std::pair<int, std::set<int>>(5, {}));
     graph.emplace(std::pair<int, std::set<int>>(6, {}));
-    const std::vector<int> &path = DFSRecursive(graph, 1, 5);
-    ASSERT_EQ(5, path.size());
+    const std::vector<int> &path = BFS(graph, 1, 5);
+    ASSERT_EQ(3, path.size());
     EXPECT_EQ(path[0], 1);
-    EXPECT_EQ(path[1], 2);
-    EXPECT_EQ(path[2], 3);
-    EXPECT_EQ(path[3], 4);
-    EXPECT_EQ(path[4], 5);
+    EXPECT_EQ(path[1], 3);
+    EXPECT_EQ(path[2], 5);
 }
--- UnitTests/NonEPI/DFSTests.cpp
+++ UnitTests/NonEPI/DFSTests.cpp
@@ -1,22 +1,22 @@
 //
-//  BFSTests.cpp
+//  DFSTests.cpp
 //  EPI
 //
-//  Created by Chris (Murim) Park on 5/1/18.
+//  Created by Chris (Murim) Park on 4/29/18.
 //  Copyright © 2018 Chris (Murim) Park. All rights reserved.
 //
 
 #include "gtest/gtest.h"
-#include "../../EPI/Graphs/General/BFS.hpp"
+#include "../../EPI/NonEPI/DFS/DFS.hpp"
 
-TEST(BFSTests, BFS_GivenEmptyGraph_ReturnsEmptyPath)
+TEST(DFSTests, DFSRecursive_GivenEmptyGraph_ReturnsEmptyPath)
 {
     std::unordered_map<int, std::set<int>> graph;
-    const std::vector<int> &path = BFS(graph, 0, 1);
+    const std::vector<int> &path = DFSRecursive(graph, 0, 1);
     EXPECT_EQ(0, path.size());
 }
 
-TEST(BFSTests, BFS_GivenValidGraph_ReturnsCorrectPath)
+TEST(DFSTests, DFSRecursive_GivenValidGraph_ReturnsCorrectPath)
 {
     std::unordered_map<int, std::set<int>> graph;
     graph.emplace(std::pair<int, std::set<int>>(1, {2, 3, 6}));
@@ -25,9 +25,11 @@ TEST(BFSTests, BFS_GivenValidGraph_ReturnsCorrectPath)
     graph.emplace(std::pair<int, std::set<int>>(4, {5}));
     graph.emplace(std::pair<int, std::set<int>>(5, {}));
     graph.emplace(std::pair<int, std::set<int>>(6, {}));
-    const std::vector<int> &path = BFS(graph, 1, 5);
-    ASSERT_EQ(3, path.size());
+    const std::vector<int> &path = DFSRecursive(graph, 1, 5);
+    ASSERT_EQ(5, path.size());
     EXPECT_EQ(path[0], 1);
-    EXPECT_EQ(path[1], 3);
-    EXPECT_EQ(path[2], 5);
+    EXPECT_EQ(path[1], 2);
+    EXPECT_EQ(path[2], 3);
+    EXPECT_EQ(path[3], 4);
+    EXPECT_EQ(path[4], 5);
 }
